733cb94f1970e653b92cea79016523e88fac878d
Muhammad Naufal <mnaufal0999@gmail.com>
2021-12-30 15:20:16 +0700

    initial commit

---
+++ app/App/Auth.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace BadHabit\LoginManagement\App;
+
+class Auth
+{
+
+}
---
+++ test/App/AuthTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace BadHabit\LoginManagement\App;
+
+use PHPUnit\Framework\TestCase;
+
+class AuthTest extends TestCase
+{
+
+}
