953b2962267bdbe3fcc81d2e099fe3de3c86edeb
Muhammad Naufal <mnaufal0999@gmail.com>
2021-12-31 11:05:24 +0700

    modified sql query

--- database.sql
+++ database.sql
@@ -1,37 +1,22 @@
--- Create database
--- For user management
 CREATE DATABASE IF NOT EXISTS `login-management-jwt`;
+CREATE DATABASE IF NOT EXISTS `login-management-test-jwt`;
 
--- Table for user management
 CREATE TABLE IF NOT EXISTS `login-management-jwt`.`users`
 (
-    `id`       VARCHAR(255) NOT NULL,
-    `name`     VARCHAR(255) NOT NULL,
+    `username` VARCHAR(255) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`id`)
+    `fullName` VARCHAR(255) NOT NULL,
+    `email`    VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`username`),
+    UNIQUE KEY `email_key` (`email`)
 ) ENGINE = InnoDB;
-
-ALTER TABLE `login-management-jwt`.`users`
-    RENAME COLUMN `id` TO `username`,
-    MODIFY COLUMN `password` VARCHAR(255) NOT NULL AFTER `username`,
-    RENAME COLUMN `name` TO `fullName`,
-    ADD COLUMN `email` VARCHAR(255) NOT NULL AFTER `fullName`;
-
--- Create database
--- For testing purposes
-CREATE DATABASE IF NOT EXISTS `login-management-test-jwt`;
 
 CREATE TABLE IF NOT EXISTS `login-management-test-jwt`.`users`
 (
-    `id`       VARCHAR(255) NOT NULL,
-    `name`     VARCHAR(255) NOT NULL,
+    `username` VARCHAR(255) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`id`)
+    `fullName` VARCHAR(255) NOT NULL,
+    `email`    VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`username`),
+    UNIQUE KEY `email_key` (`email`)
 ) ENGINE = InnoDB;
-
-ALTER TABLE `login-management-test-jwt`.`users`
-    RENAME COLUMN `id` TO `username`,
-    MODIFY COLUMN `password` VARCHAR(255) NOT NULL AFTER `username`,
-    RENAME COLUMN `name` TO `fullName`,
-    ADD COLUMN `email` VARCHAR(255) NOT NULL AFTER `fullName`;
-
